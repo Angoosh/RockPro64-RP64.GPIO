44ae96ee9dacea711272b63d5e70713531b46399
Angoosh <kaplanantonin@gmail.com>
2019-01-08 23:28:37 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="RP64.GPIO",
+    version="0.0.1",
+    author="Angoosh",
+    author_email="angoosh3d@gmail.com.com",
+    description="RockPro64 GPIO package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Angoosh/RockPro64-RP64.GPIO",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: GNU GPLv3",
+        "Operating System :: OS Independent",
+    ],
+)
